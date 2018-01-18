fca584facb3cfa426c0c64b2d39f23ecacc8bd4b
Georgiy Starkov <gstarkov658@gmail.com>
2018-01-18 16:18:49 +0300

    Добавиn polyfill Array.from для работы с IE11

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,5 +1,7 @@
 import Moment from 'moment';
 import {extendMoment} from 'moment-range';
+const arrayFrom = require('array-from');
+
 Moment.locale('ru', {
 	monthsShort: [
 		'Янв',
@@ -61,33 +63,33 @@ export const nearestFutureMinutes = (interval, someMoment) => {
 function firstMonth() {
 	const start = Moment().subtract(1, 'months').startOf('month');
 	const end = Moment().subtract(1, 'months').endOf('month');
-	return Array.from(moment.range(start, end).by('day'));
+	return arrayFrom(moment.range(start, end).by('day'));
 }
 function secondMonth() {
 	const start = Moment().startOf('month');
 	const end = Moment().endOf('month');
-	return Array.from(moment.range(start, end).by('day'));
+	return arrayFrom(moment.range(start, end).by('day'));
 }
 function thirdMonth() {
 	const start = Moment().add(1, 'months').startOf('month');
 	const end = Moment().add(1, 'months').endOf('month');
-	return Array.from(moment.range(start, end).by('day'));
+	return arrayFrom(moment.range(start, end).by('day'));
 }
 
 export const day = () => {
 	const start = Moment().startOf('day').add(7, 'hours');
 	const end = Moment().endOf('day');
-	return Array.from(moment.range(start, end).by('hours'));
+	return arrayFrom(moment.range(start, end).by('hours'));
 }
 
 export const dayInMinutes = (selectedDay) => {
 	const start = Moment(selectedDay).startOf('day').add(7, 'hours');
 	const end = Moment(selectedDay).endOf('day');
-	return Array.from(moment.range(start, end).by('minutes', {step: 15}));
+	return arrayFrom(moment.range(start, end).by('minutes', {step: 15}));
 }
 
 export const eventDuration = (dateStart, dateEnd) => {
-	return Array.from(moment.range(dateStart, dateEnd.clone().subtract(1, 'minutes')).by('minutes')).length;
+	return arrayFrom(moment.range(dateStart, dateEnd.clone().subtract(1, 'minutes')).by('minutes')).length;
 }
 
 export const isSame = (day) => Moment(Moment()).isSame(day, 'day');
